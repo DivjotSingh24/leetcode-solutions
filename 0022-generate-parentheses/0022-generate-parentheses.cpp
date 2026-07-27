5fefa0f98b7362457bac25a3a59b92bb38552fdb
Divjot Singh Gulati <146051571+DivjotSingh24@users.noreply.github.com>
2026-07-27 11:06:36 +0530

    Time: 0 ms (100%), Space: 13.1 MB (84.05%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void solve(int open, int close, int n, string &s, vector<string> &ans){
+        if(s.length()==2*n){
+            ans.push_back(s);
+            return;
+        }
+        if(open<n){
+            s.push_back('(');
+            solve(open+1,close,n,s,ans);
+            s.pop_back();
+        }
+        if(close<open){
+            s.push_back(')');
+            solve(open,close+1,n,s,ans);
+            s.pop_back();
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string> ans;
+        string s="";
+        solve(0,0,n,s,ans);
+        return ans;
+    }
+};
