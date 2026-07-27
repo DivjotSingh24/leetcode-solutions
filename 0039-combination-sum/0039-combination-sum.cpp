b3b13c06dd5c5703585b79b8e19285615c847cb0
Divjot Singh Gulati <146051571+DivjotSingh24@users.noreply.github.com>
2026-07-27 12:06:19 +0530

    Time: 0 ms (100%), Space: 14.2 MB (59.17%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    void solve(int index,int target, vector<int>& arr, vector<vector<int>>& ans, vector<int>& ds){
+        // combination found
+        if(target == 0){
+            ans.push_back(ds);
+            return;
+        }
+        // no elements left
+        if(index == arr.size())
+            return;
+
+        // take current element
+        if(arr[index] <= target){
+            ds.push_back(arr[index]);
+            // stay on same index
+            solve(index, target-arr[index], arr, ans, ds);
+            // backtrack
+            ds.pop_back();
+        }
+        // don't take current element
+        solve(index+1, target, arr, ans, ds);
+
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target){
+
+        vector<vector<int>> ans;
+        vector<int> ds;
+        solve(0,target,candidates,ans,ds);
+
+        return ans;
+    }
+};
