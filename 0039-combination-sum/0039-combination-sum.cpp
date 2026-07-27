da768ee00104494f4e8a0535c714bddc92bb4a63
Divjot Singh Gulati <146051571+DivjotSingh24@users.noreply.github.com>
2026-07-27 12:07:32 +0530

    Time: 0 ms (100%), Space: 14 MB (93.97%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -10,25 +10,20 @@ public:
         // no elements left
         if(index == arr.size())
             return;
-
         // take current element
         if(arr[index] <= target){
             ds.push_back(arr[index]);
             // stay on same index
             solve(index, target-arr[index], arr, ans, ds);
-            // backtrack
             ds.pop_back();
         }
         // don't take current element
         solve(index+1, target, arr, ans, ds);
-
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target){
-
         vector<vector<int>> ans;
         vector<int> ds;
         solve(0,target,candidates,ans,ds);
-
         return ans;
     }
 };
